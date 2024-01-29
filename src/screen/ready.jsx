39ccec08813575b2a50b1c4619be32b5d197531f
Berdi Gumantara <btara@student.gunadarma.ac.id>
2024-01-29 18:18:59 +0700

    fixing form

--- src/screen/ready.jsx
+++ src/screen/ready.jsx
@@ -2,57 +2,68 @@ import useTimer from "../hooks/useTimer";
 import useModel from "../hooks/useModel";
 import { useState } from "react";
 
-const ReadyScreen =() => {
- const{
-  modelLeft,
-  theme,
-  model,
-  setModelLeft,
-  getTheme,
-  getModel,
-  chooseTheme,
-  resetTheme,
- } = useModel();
+const ReadyScreen = () => {
+  const {
+    modelLeft,
+    theme,
+    model,
+    setModelLeft,
+    getTheme,
+    getModel,
+    chooseTheme,
+    resetTheme,
+    provideThemes,
+  } = useModel();
 
- const [value, setValue] = useState(10);
+  const [value, setValue] = useState(10);
+  const [selectedTheme, setSelectedTheme] = useState("");
 
- const handleChangeValue = (e) => {
-  setValue(e.target.value);
- }
+  const handleChangeValue = (e) => {
+    setValue(e.target.value);
+  };
 
- const handleChangeTheme = (e) => {
-  chooseTheme(e.target.value);
- }
+  const handleChangeTheme = (e) => {
+    setSelectedTheme(e.target.value);
+    console.log("selected theme", selectedTheme);
+  };
 
- const handleSubmit = () => {
-  setModelLeft(parseInt(value, 10));
-  getTheme();
-  getModel();
-  console.log('model left',modelLeft);
-  console.log('tema',theme);
-  console.log('model',model);
- }
- console.log('1model left',modelLeft);
- console.log('2tema',theme);
- console.log('3model',model);
- return (
-  <div>
-    <label>
-    Number of Moderls:
-    <input type="number" value={value} onChange={handleChangeValue} />
-    </label>
-    <br />
-    <label htmlFor="">Theme :
-      <label htmlFor="">Male
-      <input type="radio" name="theme" value="male" onChecked={theme === "male"} onChange={handleChangeTheme} />  
-      </label>
-      <label htmlFor="">Female
-      <input type="radio" name="theme" value="female" onChecked={theme === "female"} onChange={handleChangeTheme} />
-      </label>
-    </label>
-    <br />
-    <button onClick={handleSubmit}> Apply</button>
-  </div>
- )
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setModelLeft(parseInt(value, 10));
+    chooseTheme(selectedTheme);
+    getTheme();
+    getModel();
+    console.log("model left", modelLeft);
+    console.log("tema", theme);
+    console.log("model", model);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>
+          Number of Moderls:
+          <input type="number" value={value} onChange={handleChangeValue} />
+        </label>
+      </div>
+      <div>
+        <label>Choose Theme :</label>
+        {provideThemes.map((themeOption) => (
+          <label key={themeOption}>
+            {themeOption}
+            <input
+              type="radio"
+              name="theme"
+              value={themeOption}
+              onChecked={selectedTheme === themeOption}
+              onChange={handleChangeTheme}
+            />
+          </label>
+        ))}
+      </div>
+      <div>
+        <button type="submit">Apply</button>
+      </div>
+    </form>
+  );
+};
 export default ReadyScreen;
